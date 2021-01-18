2d6005b2f7971afa6d5cde6ecb777d06279c805d
Zachary Daniels <Zachary Daniels@DESKTOP-AMDA1IF>
2021-01-18 16:22:10 -0700

    Added letter grade to grade calculations

--- wwwroot/js/gradeCalculator.js
+++ wwwroot/js/gradeCalculator.js
@@ -17,5 +17,34 @@
     var finalG = wAssignment + wGroup + wQuiz + wExam + wIntex;
     finalG = Math.floor(finalG);
 
-    document.getElementById("result").value = finalG;
+/*Calculate final weighted letter grade*/
+    var letterG = "";
+    if (finalG >= 94) {
+        letterG = "A"
+    } else if (finalG >= 90 && finalG < 94) {
+        letterG = "A-"
+    } else if (finalG >= 87 && finalG < 90) {
+        letterG = "B+"
+    } else if (finalG >= 84 && finalG < 87) {
+        letterG = "B"
+    } else if (finalG >= 80 && finalG < 84) {
+        letterG = "B-"
+    } else if (finalG >= 77 && finalG < 80) {
+        letterG = "C+"
+    } else if (finalG >= 74 && finalG < 77) {
+        letterG = "C"
+    } else if (finalG >= 70 && finalG < 74) {
+        letterG = "C-"
+    } else if (finalG >= 67 && finalG < 70) {
+        letterG = "D+"
+    } else if (finalG >= 64 && finalG < 67) {
+        letterG = "D"
+    } else if (finalG >= 60 && finalG < 64) {
+        letterG = "D-"
+    } else if (finalG < 60) {
+        letterG = "E"
+    }
+    
+    document.getElementById("result1").value = finalG;
+    document.getElementById("result2").value = letterG;
 }
