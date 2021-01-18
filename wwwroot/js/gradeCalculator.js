e0201396839e7f263445d25a24f1e6dacf1c3d8b
Zachary Daniels <Zachary Daniels@DESKTOP-AMDA1IF>
2021-01-18 16:06:09 -0700

    Add project files.

---
+++ wwwroot/js/gradeCalculator.js
@@ -0,0 +1,21 @@
+﻿function gradeCalculator() {
+
+    var assignments = document.getElementById("assignments").value;
+    var groupP = document.getElementById("groupP").value;
+    var quiz = document.getElementById("quizzes").value;
+    var exams = document.getElementById("exams").value;
+    var intex = document.getElementById("intex").value;
+
+    /*Calculate weighted grades*/
+    var wAssignment = assignments * .50;
+    var wGroup = groupP * .1;
+    var wQuiz = quiz * .1;
+    var wExam = exams * .2;
+    var wIntex = intex * .1;
+
+    /*calculate final weighted grade*/
+    var finalG = wAssignment + wGroup + wQuiz + wExam + wIntex;
+    finalG = Math.floor(finalG);
+
+    document.getElementById("result").value = finalG;
+}
